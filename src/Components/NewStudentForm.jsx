f7f542df9fb8554be4daccac4fee906bb8204f0c
Robert Smith <rssmith614@gmail.com>
2023-10-03 09:13:20 -0700

    Emergency Contacts as list

--- src/Components/NewStudentForm.jsx
+++ src/Components/NewStudentForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // to be replaced with DB call
 let tutors = ["Robert Smith"]
 
@@ -10,7 +12,46 @@ function tutorOptions() {
 }
 
 const NewStudentForm = () => {
+  const [emergencyContacts, setEmergencyContacts] = useState([]);
+
+  function addEContact() {
+    setEmergencyContacts([...emergencyContacts, {id: emergencyContacts.length, name:"", relation:"", phone:""}]);
+  }
+
+  function removeEContact(id) {
+    const newList = emergencyContacts.filter(contact => contact.id !== id);
+    setEmergencyContacts(newList);
+  }
+  
+  function eContacts() {
+    return emergencyContacts.map((contact, i) => {
+      let rowid = "contact" + i;
+      return (
+        <div className="row" key={i}>
+          <div className="col mb-1">
+            <label for={rowid + 'name'} class="form-label">Name</label>
+            <input type="text" class="form-control" id={rowid + 'name'} defaultValue={contact.name} />
+          </div>
+          <div className="col mb-1">
+            <label for={rowid + 'rel'} class="form-label">Relation</label>
+            <input type="text" class="form-control" id={rowid + 'rel'} defaultValue={contact.relation} />
+          </div>
+          <div className="col mb-1">
+            <label for={rowid + 'phone'} class="form-label">Phone</label>
+            <input type="tel" class="form-control" id={rowid + 'phone'} defaultValue={contact.phone} />
+          </div>
+          {/* <div className="col mb-1 position-relative">
+            <button type="button" onClick={() => removeEContact(contact.id)}
+              class="form-control btn btn-danger
+              position-absolute bottom-0">Remove</button>
+          </div> */}
+        </div>
+      );
+    });
+  }
+
   return (
+    <>
     <form>
       <div className="row">
         <div class="col mb-3">
@@ -63,13 +104,18 @@ const NewStudentForm = () => {
         <label for="medicalConditions" class="form-label">Medical Conditions</label>
         <textarea class="form-control" id="medicalConditions" />
       </div>
-      <div class="mb-3">
-        <label for="Emergency Contacts" class="form-label">Emergency Contacts</label>
-        <textarea class="form-control" id="Emergency Contacts" />
+      <span className="mb-3">Emergency Contacts</span>
+      <div class="mx-3 mb-3">
+        {eContacts()}
       </div>
+      <button className="btn btn-secondary mb-3" type="button" onClick={addEContact}>Add New Emergency Contact</button>
 
-      <button type="submit" class="btn btn-primary position-end">Submit</button>
+      <br/>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+
+    
+    </>
   );
 }
 
