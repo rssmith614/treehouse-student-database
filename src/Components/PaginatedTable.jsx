3d8c59d56c12d238ade906a3e4e7efa1900e9335
Robert Smith <rssmith614@gmail.com>
2024-11-12 15:59:32 -0800

    Evals are now paginated with firestore

--- src/Components/PaginatedTable.jsx
+++ src/Components/PaginatedTable.jsx
@@ -1,21 +1,8 @@
 import { useEffect, useState } from "react";
-import {
-  Button,
-  Dropdown,
-  Form,
-  InputGroup,
-  Pagination,
-  Row,
-  Table,
-} from "react-bootstrap";
+import { Button, Pagination, Table } from "react-bootstrap";
 import { useMediaQuery } from "react-responsive";
 
-const PageNavigation = ({
-  numRecords,
-  pageLimit,
-  cursorIndex,
-  setCursorIndex,
-}) => {
+const PageNavigation = ({ numRecords, pageLimit, setCursorIndex }) => {
   const [pageNumber, setPageNumber] = useState(1);
 
   const maxPage = Math.floor((numRecords - 1) / pageLimit) + 1;
@@ -82,36 +69,18 @@ const PageNavigation = ({
 };
 
 const PaginatedTable = ({
-  tableType = "table",
   header,
   records,
   pageLimit,
-  tableSort,
-  setTableSort,
-  filterBy,
-  filter,
-  setFilter,
   filtered,
   clearFilters,
 }) => {
   const [cursorIndex, setCursorIndex] = useState(0);
 
-  const [dropdownLabel, setDropdownLabel] = useState("Newest First");
-
-  const isDesktop = useMediaQuery({ query: "(min-width: 992px)" });
-
   useEffect(() => {
     if (cursorIndex >= records.length) setCursorIndex(0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [records.length]);
-
-  useEffect(() => {
-    if (tableSort === "date_desc") {
-      setDropdownLabel("Newest First");
-    } else {
-      setDropdownLabel("Oldest First");
-    }
-  }, [tableSort]);
 
   return (
     <div className='d-flex flex-column'>
@@ -132,48 +101,10 @@ const PaginatedTable = ({
           </>
         ) : null}
       </div>
-      {tableType === "table" ? (
-        <Table striped hover style={{ tableLayout: "fixed" }}>
-          {header}
-          <tbody>{records.slice(cursorIndex, cursorIndex + pageLimit)}</tbody>
-        </Table>
-      ) : (
-        <>
-          <Row className={`my-1 ${isDesktop && "w-50"}`}>
-            <Dropdown>
-              <Dropdown.Toggle variant='secondary'>
-                {dropdownLabel}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setTableSort("date_desc")}>
-                  Newest First
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => setTableSort("date_asc")}>
-                  Oldest First
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            {/* <div className={`col col-${isDesktop ? "7" : "7"}`}>
-            <InputGroup>
-              <Form.Control
-                type='text'
-                placeholder={`Search ${filterBy === "tutor" ? "Student" : "Tutor"}`}
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
-              />
-              <i
-                className='bi bi-x-lg input-group-text btn btn-secondary'
-                style={{ cursor: "pointer" }}
-                onClick={() => setFilter("")}
-              />
-            </InputGroup>
-          </div> */}
-          </Row>
-          <ul className='list-group mb-3'>
-            {records.slice(cursorIndex, cursorIndex + pageLimit)}
-          </ul>
-        </>
-      )}
+      <Table striped hover style={{ tableLayout: "fixed" }}>
+        {header}
+        <tbody>{records.slice(cursorIndex, cursorIndex + pageLimit)}</tbody>
+      </Table>
       <PageNavigation
         numRecords={records.length}
         pageLimit={pageLimit}
